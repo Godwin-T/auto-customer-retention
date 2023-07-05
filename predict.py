6e9ed81aa37a5f7fdb898c130b5a2775999f2936
Godwin <godwinolagunju0@gmail.com>
2023-07-05 13:19:01 -0400

    modify prediction script to use mlflow model

--- predict.py
+++ predict.py
@@ -10,37 +10,25 @@ from flask import Flask, request, jsonify
 
 MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
-logged_model = 'runs:/b5eb75916ba045bc99b4902ea507a319/model'
+logged_model = 'runs:/9f00a33d4d4d43c1969d03d106fbf4d7/model'
 
 # Load model
 loaded_model = mlflow.pyfunc.load_model(logged_model)
 print('Connected')
 
-utils_path = 'Churn.bin'
-with open(utils_path, 'rb') as f:
-    model, encoder = pickle.load(f)
-
-
-def data_loader(path):
-    
+def load_data(path):
     data = pd.read_csv(path)
     data.columns = data.columns.str.replace(' ', '_').str.lower()
 
     categorical_col = data.dtypes[data.dtypes == 'object'].index.tolist()
     for col in categorical_col:
         data[col] = data[col].str.replace(' ', '_').str.lower()
-    return data
-
-def data_encoder(data, feature_cols):
-
-    data = encoder.transform(data[feature_cols].to_dict(orient = 'records'))
-    return data
-
-
-def file_data_prep(data):
 
     data = data[data['totalcharges'] != '_']
     data['totalcharges'] = data['totalcharges'].astype('float32')
+    return data
+
+def prepare_data(data):
 
     categorical_col = data.dtypes[data.dtypes == 'object'].index.tolist()
     numerical_col = ['tenure', 'totalcharges', 'monthlycharges']
@@ -48,7 +36,9 @@ def file_data_prep(data):
     categorical_col.remove('customerid')
     feature_cols = categorical_col + numerical_col
 
-    return data, feature_cols
+    df_data = data[feature_cols].to_dict(orient = 'records')
+    return data, df_data
+
 
 app = Flask('Churn')
 
@@ -56,15 +46,14 @@ app = Flask('Churn')
 def predict():
 
     customer = request.get_json()
-    data = data_loader(customer)
-    data, feature_cols = file_data_prep(data)
-    encoded_data = data_encoder(data, feature_cols)
-    prediction = loaded_model.predict(encoded_data)
+    data = load_data(customer)
+    data, df = prepare_data(data)
+    #encoded_data = data_encoder(data, feature_cols)
+    prediction = loaded_model.predict(df)
     data['churn'] = prediction
     output = data[data['churn'] == 1]
     output.to_csv('data/churn.csv', index = False)
     output_text = {'Prediction Status': 'The model successufully predicted the customers that are likely to churn and save the file'}
-    output = {'Prediction Shape':prediction.shape}
     return jsonify(output_text)
 
 if __name__ == '__main__':

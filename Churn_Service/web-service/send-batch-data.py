efaf8c1c192443713ea11026c1d77e7a3478ba2d
Godwin <godwinolagunju0@gmail.com>
2024-01-03 10:05:44 +0100

    reformatted file

--- Churn_Service/web-service/send-batch-data.py
+++ Churn_Service/web-service/send-batch-data.py
@@ -4,7 +4,7 @@ import requests
 child_directory = os.getcwd()
 parent_directory = os.path.dirname(child_directory)
 
-data_path = f'{parent_directory}/data/new-data.csv'
+data_path = f"{parent_directory}/data/new-data.csv"
 url = "http://127.0.0.1:5080/predict"
-response = requests.post(url, json = data_path).json()
+response = requests.post(url, json=data_path).json()
 print(response)
--- Churn_Service/web-service/send-data.py
+++ Churn_Service/web-service/send-data.py
@@ -1,6 +1,6 @@
 # Import Libraries
 
-import os 
+import os
 import json
 from datetime import datetime
 
@@ -11,12 +11,13 @@ import requests
 
 child_dir = os.getcwd()
 parent_dir = os.path.dirname(child_dir)
-file_name = 'Telco-Customer-Churn.csv'
+file_name = "Telco-Customer-Churn.csv"
 
-file_path = f'{parent_dir}/data/{file_name}'
+file_path = f"{parent_dir}/data/{file_name}"
 
 data = pd.read_csv(file_path)
-data = data.to_dict(orient= 'records')
+data = data.to_dict(orient="records")
+
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, o):
@@ -26,11 +27,13 @@ class DateTimeEncoder(json.JSONEncoder):
 
 
 # Send Data
-with open("target.csv", 'w') as f_target:
+with open("target.csv", "w") as f_target:
 
     url = "http://127.0.0.1:9696/predict"
     for row in data:
-        print('===============')
-        response = requests.post(url, json = row).json() 
-        print(f"Customer ID: {response['customerid']}      Prediction: {response['churn']}")
+        print("===============")
+        response = requests.post(url, json=row).json()
+        print(
+            f"Customer ID: {response['customerid']}      Prediction: {response['churn']}"
+        )
         sleep(1)

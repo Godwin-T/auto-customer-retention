bcee4b1eccb6d43ecee4a93b056b063d9d8d3d84
Godwin <godwinolagunju0@gmail.com>
2024-01-11 23:33:35 +0100

    remve api key

--- LLM_Service/scripts/constant.py
+++ LLM_Service/scripts/constant.py
@@ -2,5 +2,5 @@ RAW_DATAPATH = "../raw_data/"
 DATA_PATH = "../processed_data/mails.csv"
 MODEL_NAME = "text-davinci-003"
 
-API_KEY = "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"
+API_KEY = ""
 COMPLETION_MODEL_NAME = "gpt-3.5-turbo-instruct"

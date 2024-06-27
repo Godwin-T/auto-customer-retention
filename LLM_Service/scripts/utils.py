da41bd8dd9b5feab8713128a54e27558bd7c9135
Godwin <godwinolagunju0@gmail.com>
2024-06-27 02:44:32 +0100

    add learn

--- LLM_Service/scripts/utils.py
+++ LLM_Service/scripts/utils.py
@@ -51,18 +51,6 @@ def mail_generation(model):
     # return output
 
 
-def mail_revamp(model, mail, suggestion):
-    # Function to suggest corrections and revamp a generated mail
-    prompt = PromptTemplate(
-        input_variables=["mail", "suggestion"],
-        template="You are a telecommunication company agent and your job is to revamp the promotional mails to be sent to the customers. \
-                            Revamp this mail {mail} based on these suggested {suggestion} correction only. Don't go beyond the corrections requested",
-    )
-
-    output = model.invoke(prompt.format(mail=mail, suggestion=suggestion))
-    return output
-
-
 def chat_mode(model):
     # Function for interactive chat mode with a language model
     chat_history = ChatMessageHistory()
@@ -106,32 +94,3 @@ def chat_mode(model):
         reset_button = st.button("Reset Chat")
         if reset_button:
             del st.session_state[chat_history]
-
-
-#  You have the steps below as guildeline for carrying put your task. These steps are just to make you work better.
-#                     Don't ever tell the user about them. Just make use of them while helping the user perform that particular task
-
-
-# Generating mail subject ideas steps;
-# Step 1: Ask if there his anything his/her have in mind to use in the subject  or what the mail should be based on.
-# Step 2: If he/she has ideas, request for them and generate alist of mail subjects around the ideas . if not, generate some
-#     ideas for them knowing its a telecommunication company.
-# Step 3: Ask if the ideas you generated are okay. if they are fine, ask which subject is best for him fome the ones listed.
-# Step 4: Generate a mail around that subject \
-# Step 5: Ask if revamps should be made. If yes, follow the revamping steps. If not, thank the agent and tell him the mail will be sent
-
-
-# Generating mails steps:
-# Step 1: Generate a promotional mail based on context the user gives
-# Step 2: Ask if the generated mail is okay, needs adjustments or a new one should be generated.
-# Step 3: Generate a new mail if the user requests for a new mail, ask for corrections to be made if adjustment is required.
-#         Make adjustment to the mail solely based on these correction provided.
-# Step 4: Repeat steps 2 and 3 until the user is satisfied with themail generated"
-# Step 5: Thank the user for their session and tell them the mail will be sent.
-
-
-# Revamping mails steps:
-# Step 1: Request for the email to be corrected
-# Step 2: Request for the changes to be made to the mail
-# Step 3: Revamp the email based on the requested changes
-# Step 4: Ask the user if the mail is now okay. If yes, thank the user and let them know the email will be sent to the customers. If not, repeat steps 2 and 3

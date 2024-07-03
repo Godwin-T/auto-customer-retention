7a3b8d1ec3f925beab596a3efbad0fb782f1b447
Godwin <godwinolagunju0@gmail.com>
2024-07-03 23:53:27 +0100

    change model helper name

--- ChurnGuard/training_pipeline/train.py
+++ ChurnGuard/training_pipeline/train.py
@@ -5,7 +5,7 @@ import pandas as pd
 from prefect import task, flow
 from sklearn.model_selection import train_test_split
 
-from trainhelpers import evaluate_model, train_model, save_model
+from modelhelper import evaluate_model, train_model, save_model
 from utils import (
     save_metrics,
     save_predictions,

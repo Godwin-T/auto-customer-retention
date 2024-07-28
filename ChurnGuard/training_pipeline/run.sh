7df8b94b4d6a76f8496a2b223cca56543d7ceb66
Godwin <godwinolagunju0@gmail.com>
2024-07-28 18:50:41 +0100

    remove mlflow server

--- ChurnGuard/training_pipeline/run.sh
+++ ChurnGuard/training_pipeline/run.sh
@@ -5,3 +5,5 @@ python process_data.py
 
 # Run the training script
 python train.py
+
+#mlflow server --backend-store-uri sqlite:///./databases/mlflow.db --default-artifact-root file:///./databases/mlruns/artifacts/model --host 0.0.0.0 --port 5000

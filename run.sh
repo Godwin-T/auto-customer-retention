6986a08fced05e8f2208464d6a6c66ce7f4de0b7
Godwin <godwinolagunju0@gmail.com>
2024-10-18 23:16:10 +0100

    add model run

---
+++ run.sh
@@ -0,0 +1,16 @@
+DBNAME=${{ secrets.DBNAME }}
+MYSQL_USERNAME=${{ secrets.MYSQL_USERNAME }}
+MYSQL_PASSWORD=${{ secrets.MYSQL_PASSWORD }}
+HOSTNAME=${{ secrets.HOSTNAME }}
+
+cat <<EOT > .env
+DBNAME=$DBNAME
+MYSQL_USERNAME=$MYSQL_USERNAME
+MYSQL_PASSWORD=$MYSQL_PASSWORD
+HOSTNAME=$HOSTNAME
+EOT
+
+python_output=$(python run.py)
+
+# Set the output of the Python script as an environment variable
+DEPLOY=$python_output

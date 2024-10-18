354c0fe6568395208b2aad72a7503091b2c79932
Godwin <godwinolagunju0@gmail.com>
2024-10-18 23:47:41 +0100

    update run

--- run.sh
+++ run.sh
@@ -13,5 +13,10 @@ EOT
 
 python_output=$(python run.py)
 
+echo $HOSTNAME
+echo $MYSQL_PASSWORD
+echo $MYSQL_USERNAME
+exho $DBNAME
+
 # Set the output of the Python script as an environment variable
 DEPLOY=$python_output

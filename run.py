c2e50dd53a7dfc65e1577ebff650f99299326ac4
Godwin <godwinolagunju0@gmail.com>
2024-10-19 21:29:15 +0100

    update workflow

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app.churn_guard.train_pipeline.train import training_pipeline
 from app.churn_guard.utils.deploy import deploy_production
 
 training_pipeline()
-# print("Successful")
+# print("Successful")0
 result = deploy_production()

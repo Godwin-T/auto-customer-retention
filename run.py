80a67678d735933cc2e0610ce8f865dda9af413a
Godwin <godwinolagunju0@gmail.com>
2024-10-20 05:16:17 +0100

    update

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ from app.churn_guard.train_pipeline.train import training_pipeline
 from app.churn_guard.utils.deploy import deploy_production
 
 training_pipeline()
-# print("Successful")0
+# print("Successful")
 result = deploy_production()

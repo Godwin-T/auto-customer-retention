3d2e237d4405897b1022a13d29f955ba4666649c
Godwin <godwinolagunju0@gmail.com>
2024-10-21 15:05:39 +0100

    return to base

--- app/churn_guard/utils/deploy.py
+++ app/churn_guard/utils/deploy.py
@@ -15,26 +15,32 @@ client = MlflowClient(tracking_uri=tracking_uri)
 def change_model_stage():
 
     production_alias = "Production"
-    staged_alias = "Staged"
 
     latest_model = client.get_registered_model(model_name).latest_versions
     latest_model_id = latest_model[0].run_id
     latest_model_version = latest_model[0].version
 
-    production_model = client.get_model_version_by_alias(model_name, production_alias)
-    production_model_id = production_model.run_id
-
-    production_metrics = get_metrics(client, production_model_id)
-    latest_metrics = get_metrics(client, latest_model_id)
-    deploy = compare_metrics(production_metrics, latest_metrics)
-
-    if deploy:
+    if latest_model_version == 1:
         client.set_registered_model_alias(
             model_name, production_alias, latest_model_version
         )
-        deploy_info = {"run_id": production_model_id, "change": True}
+        deploy_info = {"run_id": latest_model_id, "change": True}
+
     else:
-        deploy_info = {"run_id": production_model_id, "change": False}
+        production_model_id = client.get_model_version_by_alias(
+            model_name, production_alias
+        ).run_id
+        production_metrics = get_metrics(client, production_model_id)
+        latest_metrics = get_metrics(client, latest_model_id)
+        deploy = compare_metrics(production_metrics, latest_metrics)
+
+        if deploy:
+            client.set_registered_model_alias(
+                model_name, production_alias, latest_model_version
+            )
+            deploy_info = {"run_id": production_model_id, "change": True}
+        else:
+            deploy_info = {"run_id": production_model_id, "change": False}
 
     return deploy_info
 

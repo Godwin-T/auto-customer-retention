2220b16c7dd718c6937e87eb4a2fdabc55e8498e
Godwin <godwinolagunju0@gmail.com>
2024-10-21 06:35:01 +0100

    update

--- k.py
+++ k.py
@@ -3,27 +3,28 @@ import pandas as pd
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-load_dotenv()
-
 
 dbname = os.getenv("DBNAME")
-username = os.getenv("MYSQL_USERNAME")
-password = os.getenv("MYSQL_PASSWORD")
-hostname = os.getenv("THOSTNAME")
-print(dbname, username, hostname, password)
+# username = os.getenv("MYSQL_USERNAME")
+# password = os.getenv("MYSQL_PASSWORD")
+# hostname = os.getenv("THOSTNAME")
 
-engine = create_engine(
-    f"mysql+mysqlconnector://{username}:{password}@{hostname}/{dbname}"
-)
-print(dbname, username, hostname, password)
+if dbname:
+    print(f"API Key: {dbname}")
+else:
+    print("API Key not found!")
 
-
-def load_data_from_mysql_db(sql_engine, tablename):
-
-    query = f"SELECT * FROM {tablename}"
-    df = pd.read_sql(query, con=sql_engine)
-    return df
+# engine = create_engine(
+#     f"mysql+mysqlconnector://{username}:{password}@{hostname}/{dbname}"
+# )
 
 
-df = load_data_from_mysql_db(engine, "ProcessedData")
-print("True")
+# def load_data_from_mysql_db(sql_engine, tablename):
+
+#     query = f"SELECT * FROM {tablename}"
+#     df = pd.read_sql(query, con=sql_engine)
+#     return df
+
+
+# df = load_data_from_mysql_db(engine, "ProcessedData")
+# print("True")

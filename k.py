465d7692e117005a3e5ce3f70ee6701efab9c0d5
Godwin <godwinolagunju0@gmail.com>
2024-10-21 07:54:36 +0100

    update

--- k.py
+++ k.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 dbname = os.getenv("DBNAME")
 username = os.getenv("MYSQL_USERNAME")
 password = os.getenv("MYSQL_PASSWORD")
-hostname = os.getenv("THOSTNAME")
+hostname = os.getenv("HOSTNAME")
 
 if dbname:
     print(f"API Key: {dbname}")
@@ -35,12 +35,12 @@ engine = create_engine(
 )
 
 
-def load_data_from_mysql_db(sql_engine, tablename):
+# def load_data_from_mysql_db(sql_engine, tablename):
 
-    query = f"SELECT * FROM {tablename}"
-    df = pd.read_sql(query, con=sql_engine)
-    return df
+#     query = f"SELECT * FROM {tablename}"
+#     df = pd.read_sql(query, con=sql_engine)
+#     return df
 
 
-df = load_data_from_mysql_db(engine, "ProcessedData")
-print("True")
+# df = load_data_from_mysql_db(engine, "ProcessedData")
+# print("True")

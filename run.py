b5f4f01a1ccce65c4c776850e51da2a6c33c6e44
Godwin <godwinolagunju0@gmail.com>
2024-10-22 13:23:08 +0100

    add deployment flow

--- run.py
+++ run.py
@@ -2,6 +2,12 @@ from app.churn_guard.train_pipeline.train import training_pipeline
 from app.churn_guard.utils.deploy import deploy_production
 from app.churn_guard.utils.process_data import main
 
-training_pipeline()
-result = deploy_production()
-print("Succesful")
+
+# training_pipeline()
+# # result = deploy_production()
+# print("Succesful")
+
+if __name__ == "__main__":
+    training_pipeline()
+    result = deploy_production()
+    print("Succesful")

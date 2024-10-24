81e23d10f3ff4d89ae65e60ddd99299439a055f7
Godwin <godwinolagunju0@gmail.com>
2024-10-24 21:03:54 +0100

    isolate prefect function

--- tests/churn_service_tests/test_data_processing.py
+++ tests/churn_service_tests/test_data_processing.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from app.churn_guard.utils.process_data import load_dataset, process_dataset
+from app.churn_guard.utils.process_data import load_dataset, process_dataset, save_dataf
 
 
 def test_load_dataset():
@@ -7,15 +7,15 @@ def test_load_dataset():
     input_data_path = "./sample_data/raw_data.csv"
     data = load_dataset(input_data_path)
 
-    assert isinstance(data, pd.DataFrame)
+    assert isinstance.fn(data, pd.DataFrame)
     assert data.shape == (11, 21)
 
 
 def test_process_dataset():
 
     input_data_path = "./sample_data/raw_data.csv"
-    data = load_dataset(input_data_path)
-    output_data = process_dataset(data, target_column_name="churn")
+    data = load_dataset.fn(input_data_path)
+    output_data = process_dataset.fn(data, target_column_name="churn")
 
     assert isinstance(output_data, pd.DataFrame)
     assert output_data["churn"].dtype == "int"

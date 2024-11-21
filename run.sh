3a3de9481ea9ac4c31cf42ba3fdec3e69e98dfa2
Godwin <godwinolagunju0@gmail.com>
2024-11-21 15:56:33 +0100

    rm run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-echo '=================================================================================='
-python_output=$(python run.py)
-echo '=================================================================================='
-# Set the output of the Python script as an environment variable
-DEPLOY=$python_output
-echo $DEPLOY

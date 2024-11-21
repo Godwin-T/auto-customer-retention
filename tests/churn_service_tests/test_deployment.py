812d370fa2cf74fb88a180827be44626d33536ef
Godwin <godwinolagunju0@gmail.com>
2024-11-21 01:04:49 +0100

    change requirements path

--- tests/churn_service_tests/test_deployment.py
+++ tests/churn_service_tests/test_deployment.py
@@ -187,7 +187,7 @@ def test_upload_file_to_s3():
 
 def test_required_dependencies():
 
-    with open("./app/churn_guard/guard_deploy/requirements.txt") as f:
+    with open("./app/backend/churn_guard/guard_deploy/requirements.txt") as f:
         required = f.read().splitlines()
 
     installed = [pkg.key for pkg in pkg_resources.working_set]

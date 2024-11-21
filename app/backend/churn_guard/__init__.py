dd071d172ca6f3f9b10849c7467896bb75f54f8a
Godwin <godwinolagunju0@gmail.com>
2024-11-21 01:03:45 +0100

    update initialization for module

--- app/backend/churn_guard/__init__.py
+++ app/backend/churn_guard/__init__.py
@@ -1,8 +1,10 @@
 from .utils.deploy import *
-from .train_pipeline.train import *
+
 from .utils.datahelper import *
 from .utils.modelhelper import *
 from .utils.evaluate import *
 from .utils.process_data import *
+from .utils.validate import *
+
 from .guard_deploy.app import *
 from .train_pipeline.train import *

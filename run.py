8d2af01248e1b0a9b2f8f8ddeddd6c6311afc49b
Godwin <godwinolagunju0@gmail.com>
2024-11-21 15:54:15 +0100

    update run

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-from app.churn_guard.train_pipeline.train import training_pipeline
-from app.churn_guard.utils.deploy import deploy_production
-
+from app.backend.churn_guard import training_pipeline
+from app.backend.churn_guard import deploy_production
 
 if __name__ == "__main__":
     training_pipeline()

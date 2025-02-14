aab27b22e2fdfe69c8c62ddde24d0467b0198df0
Godwin-T <godwinolagunju0@gmail.com>
2025-02-14 12:41:33 +0100

    update

--- run.py
+++
@@ -1,7 +0,0 @@
-from src.backend.churn_guard import training_pipeline
-from src.backend.churn_guard import deploy_production
-
-if __name__ == "__main__":
-    training_pipeline()
-    result = deploy_production()
-    print("Succesful")
